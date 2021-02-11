9a4b97bd06c405298d7195a43f3043c0a45a35de
Ben Line <hello@benline.co.uk>
2021-02-11 00:39:11 +0000

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const postShopifyGraphQL = (query:any) => {
+  return new Promise<any>((resolve, reject) => {
+    axios({
+      headers: {
+        'X-Shopify-Access-Token': process.env.ACCESS_TOKEN,
+        'Content-Type': 'application/json',
+      },
+      url: `https://${process.env.SHOP}.myshopify.com/admin/api/2019-10/graphql.json`,
+      method: 'post',
+      data: {
+        query: query,
+      },
+    })
+    .then(result => {
+      resolve(result.data)
+    }).catch(error => {
+      reject(error)
+    })
+  });
+}
+
